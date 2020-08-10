60b7cbb5cdef8462a44cf7036bdf4927631375a6
Slava140 <69424063+Slava140@users.noreply.github.com>
2020-08-10 23:48:18 +0600

    Update control-bot.py

--- control-bot.py
+++ control-bot.py
@@ -13,7 +13,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--headless')
 
-browser = webdriver.Chrome('\\driver\\chromedriver.exe', options=options)
+browser = webdriver.Chrome('chromedriver.exe', options=options)
 browser.set_window_size(1280,1024)
 
 browser.get('https://accounts.google.com/signin/oauth/identifier?response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&openid.realm&access_type=offline&include_granted_scopes=true&redirect_uri=storagerelay%3A%2F%2Fhttps%2Faccount.habr.com%3Fid%3Dauth356479&client_id=472084966048-ut99mpprfh1n8ees0r26kr1183u1q4r0.apps.googleusercontent.com&ss_domain=https%3A%2F%2Faccount.habr.com&gsiwebsdk=shim&state=40eaf1759c7a851ef3c330f92b5d62a8&o2v=1&as=Sc0bQgml6wT_wcGPs14mkA&flowName=GeneralOAuthFlow')
